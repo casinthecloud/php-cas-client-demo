501bcb220a15be9872e91a9948c3a525838991fc
Jérôme LELEU <leleuj@gmail.com>
2024-07-29 17:56:47 +0200

    test

---
+++ www/protected/index2.php
@@ -0,0 +1,27 @@
+<?php
+	require_once '../casconfig.php';
+
+	if (isset($_SESSION["casResource"]) && $_SESSION["casResource"] !== "resource2") {
+		unset($_SESSION["phpCAS"]["user"]);
+	}
+	$_SESSION["casResource"] = "resource2";
+
+	// protected page -> force authentication
+	phpCAS::forceAuthentication();
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>php-cas-client-demo</title>
+	<meta charset="UTF-8" />
+</head>
+<body>
+	<h1>Index2</h1>
+	<a href="index1.php">Index1</a>
+	<a href="index2b.php">Index2b</a>
+	<h3>
+		<p>User: <?php echo phpCAS::getUser(); ?></p>
+		<p>Attributes: <?php print_r(phpCAS::getAttributes()); ?></p>
+	</h3>
+</body>
+</html>
